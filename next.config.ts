1e197f996e94d59e2b44ed1af32a6b70a514c4db
Thallia Borba Rosa <53798430+tliabr@users.noreply.github.com>
2024-11-22 17:18:23 +0000

    config change for the BE

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+    env: {
+        NEXT_PUBLIC_BACKEND_URL: 'http://localhost:3000',
+    },
+}
 export default nextConfig;
