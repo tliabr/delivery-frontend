6ec79890dc99a3911fd621de425153a432e93cfe
Thallia Borba Rosa <53798430+tliabr@users.noreply.github.com>
2024-11-22 18:16:50 +0000

    style mobile version

--- src/app/welcome/[id]/page.tsx
+++ src/app/welcome/[id]/page.tsx
@@ -2,6 +2,8 @@
 import {useParams} from "next/navigation";
 import {useEffect, useState} from "react";
 import {formatCurrency} from "@/utils/helpers";
+import "../../styles/Page.css";
+
 interface UserContent {
     title: string;
     message: string;
@@ -28,15 +30,35 @@ export default function Welcome() {
         }
         fetchContent();
     }, []);
-    return <>
-        {userContent === null
-            ? <p>Loading...</p>
-            : <>
-                <h1>{userContent.title}</h1>
-                <p>{userContent.message}</p>
-                <p>Total price: {formatCurrency(userContent.totalPrice)}</p>
-                {userContent.freeGift && <p>You get a free gift!</p>}
-            </>
-        }
-    </>
-}
+    return (
+        <>
+            {userContent === null ? (
+                <p className="text-center text-gray-500">Loading...</p>
+            ) : (
+                <div className="container">
+                    <div className="image-container">
+                        <img src="/my-cats.jpeg" alt="" className="image"/>
+                    </div>
+                    <div className="content">
+                        <h1 className="title">{userContent.title}</h1>
+                        <p className="message">{userContent.message}</p>
+                        <p className="total-price">Total price: {formatCurrency(userContent.totalPrice)}</p>
+                    </div>
+                    <div className="buttons">
+                        <button className="btn primary-btn">SEE DETAILS</button>
+                        <button className="btn secondary-btn">EDIT DELIVERY</button>
+                    </div>
+
+
+                    {userContent.freeGift && (
+                        <div className="free-gift">
+                            <p className="free-gift-text">FREE GIFT</p>
+                        </div>
+                    )}
+                </div>
+            )}
+        </>
+        );
+    }
+
+
